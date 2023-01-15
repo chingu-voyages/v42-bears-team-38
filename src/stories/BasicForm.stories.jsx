e3933952ed759a0f489eb3f986731ef57dd76c33
Jason Boxman <jasonb@edseek.com>
2023-01-15 14:11:17 -0500

    WIP

---
+++ src/stories/BasicForm.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BasicForm from '../components/BasicForm';
+
+export default {
+  component: BasicForm,
+}
+
+const Template = args => <BasicForm />
+
+export const Me = Template.bind({});
