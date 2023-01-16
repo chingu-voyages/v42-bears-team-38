1f8802c895912f1cd203227ea2fdfddb5b2f28c7
Jason Boxman <jasonb@edseek.com>
2023-01-16 18:53:36 -0500

    Fix bad import

--- src/stories/button/Button.jsx
+++ src/stories/button/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
-import { dive } from 'synonyms/dictionary';
 
 /**
  * Primary UI component for user interaction
