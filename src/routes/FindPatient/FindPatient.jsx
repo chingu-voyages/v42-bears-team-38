a614abdea3684996e3f9fe8f8760847f66d6dc8f
Jason Boxman <jasonb@edseek.com>
2023-02-08 15:50:27 -0500

    Ensure patient search updates the page

--- src/routes/FindPatient/FindPatient.jsx
+++ src/routes/FindPatient/FindPatient.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-import { useDispatch } from "react-redux";
-import { Button } from "../../stories/button/Button";
-import { Input } from "../../stories/input/Input";
+import { useDispatch, useSelector } from "react-redux";
+//import { Button } from "../../stories/button/Button";
+//import { Input } from "../../stories/input/Input";
+import { Button, TextInput, FormGroup } from "@patternfly/react-core";
+
 import { searchPatient } from "../../utils/perscriptionApi/perscriptionApi";
 import { setPatient } from "../../utils/store/prescriptionSlice";
 import "./findPatient.css";
@@ -10,6 +12,7 @@ import "./findPatient.css";
 const FindPatient = () => {
   const [searchEmail, setSearchEmail] = useState("");
   const dispatch = useDispatch();
+  const patient = useSelector(state => state.prescription.patient);
 
   const handleSearch = async () => {
     const patientData = await searchPatient(searchEmail);
@@ -18,13 +21,19 @@ const FindPatient = () => {
 
   return (
     <>
-      <Input
-        value={searchEmail}
-        onChange={setSearchEmail}
-        label="E-mail"
-        icon={<AiOutlineSearch />}
-      />
-      <Button label="Search" onClick={handleSearch} />
+      <form>
+        <FormGroup label="E-mail" isRequired>
+          <TextInput
+            id='search-email'
+            type="text"
+            value={searchEmail}
+            onChange={setSearchEmail}
+            icon={<AiOutlineSearch />}
+          />
+        </FormGroup>
+        <Button onClick={handleSearch}>Search</Button>
+      </form>
+      {patient && <div>{JSON.stringify(patient, null, 2)}</div>}
     </>
   );
 };
